faaadd0f70dbf8b3a56bdae3d5820c97ba2a9db6
medlinnaa <madinamamishova@gmail.com>
2025-12-15 21:36:21 +0000

    a function that replaces all occurrences of an element by another in a new list

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new _list= my_list.replace(search, replace)
+    new_list= my_list.replace(search, replace)
     return new_list
