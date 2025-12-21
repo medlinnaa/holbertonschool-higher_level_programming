244cb592a6e67bbc65e32203fda9a887c49880c4
medlinnaa <madinamamishova@gmail.com>
2025-12-21 18:33:30 +0000

    README added

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Provides a function that lists an object's available attributes."""
+
+
+def lookup(obj):
+    """Return the list of available attributes and methods of an object."""
+    return dir(obj)
+
